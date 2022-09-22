b466bf08858cea7d2439a3825dc8b3a36d141d09
cd-x <whowantsthisemailid@gmail.com>
2022-09-22 22:01:23 +0530

    redux http and notification

--- src/App.js
+++ src/App.js
@@ -2,9 +2,26 @@ import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
 import { useSelector } from "react-redux";
+import { useEffect } from "react";
 
 function App() {
   const isCartVisible = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
+  useEffect(() => {
+    const sendCartData = async () => {
+      const response = await fetch(
+        "https://redux-http-27f57-default-rtdb.firebaseio.com/cart.json",
+        {
+          method: "PUT",
+          body: JSON.stringify(cart),
+        }
+      );
+      if (!response.ok) {
+        throw new Error("Failed in sending cart data !");
+      }
+    };
+    sendCartData();
+  }, [cart]);
   return (
     <Layout>
       {isCartVisible && <Cart />}
